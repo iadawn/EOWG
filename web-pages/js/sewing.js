a0fc84ba875fb35990e6bb00f5c82019c96a1ce2
Adina Halter <haltersweb@gmail.com>
2016-03-12 20:46:10 -0500

    remove console log and edit comment

--- web-pages/js/sewing.js
+++ web-pages/js/sewing.js
@@ -1,11 +1,9 @@
 /**
  * @author Adina Halter
- * Accessible Form Error Handling
  */
 (function ($, NAME) {
     'use strict';
     $('#triggerBig').on('click', function (evt) {
-        console.log('test');
         $('.small-target').addClass('get-big');
     });
 }(jQuery, NAME));
