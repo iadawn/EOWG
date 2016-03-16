bcb38f275cc53d5a714e0480f7f639ae883b169b
Adina Halter <haltersweb@gmail.com>
2016-03-15 22:05:14 -0400

    fixed for/id pair and made labels hot only when radios are large

--- web-pages/js/checking.js
+++ web-pages/js/checking.js
@@ -4,8 +4,14 @@
 (function ($, NAME) {
     'use strict';
     $('svg+label').on('click', function (evt) {
+    	if ( $(this).prev('.svg-radio').attr('class') === "svg-radio get-big" ) { return; };
 		evt.preventDefault();
 		evt.stopPropagation();
+	});
+	$('input[type="radio"]+svg>circle:first-child').on('click', function (evt) {
+		if ( $(this).parent('svg').attr('class') === "svg-radio get-big" ) {
+			$(this).parent('svg').prev('input').click();
+		}
 	});
 	$('input[type="radio"]+svg>circle+circle').on('click', function (evt) {
 		$(this).parent('svg').prev('input').click();
