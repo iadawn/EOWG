c9ee94afdd2b7efd03277e7e446182bf2ddc4b2e
Adina Halter <haltersweb@gmail.com>
2016-03-15 21:37:21 -0400

    radios that grow and shrink

--- web-pages/js/checking.js
+++ web-pages/js/checking.js
@@ -10,4 +10,11 @@
 	$('input[type="radio"]+svg>circle+circle').on('click', function (evt) {
 		$(this).parent('svg').prev('input').click();
 	});
+	$('[type="submit"]').on('click', function (evt) {
+		evt.preventDefault();
+		$('.svg-radio').attr('class', 'svg-radio get-big');
+	});
+	$('[type="reset"]').on('click', function (evt) {
+		$('.svg-radio').attr('class', 'svg-radio');
+	});
 }(jQuery, NAME));
